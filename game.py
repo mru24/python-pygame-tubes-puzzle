7e62fdf3f8d74a9e90f429692ddad7aa622b1de3
mru24 <mru24@yahoo.com>
2024-02-23 00:07:57 +0000

    first row

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import pygame as pg
 import random as rnd
+import copy
 
 pg.init()
 
 WIDTH = 500
-HEIGHT = 500
+HEIGHT = 550
 
 screen = pg.display.set_mode([WIDTH, HEIGHT])
 pg.display.set_caption("Py game")
@@ -16,6 +17,7 @@ new_game = True
 
 tubes = 10
 colors = []
+color_choices = ['red','blue','green','purple','light green','light blue','dark green','dark blue','orange','violet','pink','grey']
 
 def generate_start():
     tubes_number = rnd.randint(10, 14)
@@ -34,6 +36,25 @@ def generate_start():
     print(tubes_colors, tubes_number)
     return tubes_number, tubes_colors
 
+def draw_tubes(tubes_num, tubes_cols):
+    tube_boxes = []
+    if tubes % 2 == 0:
+        tubes_per_row = tubes_num // 2
+        offset = False
+    else:
+        tubes_per_row = tubes_num // 2 + 1
+        offset = True
+    
+    spacing = WIDTH / tubes_per_row
+
+    for i in range(tubes_per_row):
+        for j in range(len(tubes_cols[i])):
+            pg.draw.rect(screen, color_choices[tubes_cols[i][j]], [5 + spacing*i, 200 - (50*j), 65, 50], 0, 3)
+        box = pg.draw.rect(screen, 'white', [5 + spacing*i, 50, 65, 200], 5, 3)
+        tube_boxes.append(box)
+
+    return tube_boxes
+
 run = True
 while run:
     screen.fill("black")
@@ -41,7 +62,10 @@ while run:
 
     if new_game:
         tubes, colors = generate_start()
+        initial_colors = copy.deepcopy(colors)
         new_game = False
+    else:
+        tube_rects = draw_tubes(tubes, colors)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
